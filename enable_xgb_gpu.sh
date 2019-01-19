da9c6e0ffe6196b071347ee77b332bc64fc9daa8
Ezequiel A. Pássaro <epassaro15@gmail.com>
2019-01-19 20:38:22 -0300

    Minor changes #2.

--- enable_xgb_gpu.sh
+++ enable_xgb_gpu.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script enables XGBoost GPU compatibility in Google Colab.
+
 wget https://s3-us-west-2.amazonaws.com/xgboost-wheels/xgboost-0.81-py2.py3-none-manylinux1_x86_64.whl
 
 pip uninstall -y xgboost
